7ee842f6f34a377c7d827543bef6fe664281b8fe
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-02-07 18:33:23 -0500

    fist commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,32 @@
+for x in range(151):
+    print(x)
+
+for y in range(5,1001,5):
+    print(y)
+
+
+for z in range(1,101):
+    z
+    if z % 10 == 0:
+        print("Coding Dojo")
+    elif z % 5 == 0:
+        print("Coding")
+    else:
+        print(z)
+
+sum = 0
+for q in range(1,500000):
+    if q % 2 == 1:
+        sum = sum + q
+print(sum)
+
+for i in range(2018,0,-4):
+    print(i)
+
+
+lowNum = 2
+highNum = 9
+mult = 3
+for o in range(lowNum,highNum + 1):
+    if o % mult == 0:
+        print(o)
